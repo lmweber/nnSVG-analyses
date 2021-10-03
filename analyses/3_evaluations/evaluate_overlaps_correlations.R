0c6ab9fd06a96d7f52c25b6546a0c907af006aa2
Lukas Weber <lukas.weber.edu@gmail.com>
2021-10-03 11:15:10 -0400

    calculate and plot correlations

--- analyses/3_evaluations/evaluate_overlaps_correlations.R
+++ analyses/3_evaluations/evaluate_overlaps_correlations.R
@@ -61,8 +61,8 @@ res_mOB <- cbind(res_mOB_nnSVG, res_mOB_nnSVG_clusters, res_mOB_HVGs)
 # that are also in the set of top n genes from method 2 (e.g. nnSVG)
 
 overlaps <- c(10, 20, 50, 100, 200, 500, 1000)
-top_nnSVG <- rep(NA, length(top_HVGs))
-top_nnSVG_clusters <- rep(NA, length(top_HVGs))
+top_nnSVG <- rep(NA, length(overlaps))
+top_nnSVG_clusters <- rep(NA, length(overlaps))
 
 for (k in seq_along(overlaps)) {
   # select top gene names
@@ -94,8 +94,8 @@ ggplot(df_overlaps, aes(x = top_HVGs, y = prop_overlap,
                         group = method, color = method)) + 
   geom_line() + 
   geom_point() + 
-  scale_color_ochre(palette = "parliament") + 
-  scale_x_continuous(breaks = top_HVGs, trans = "log10") + 
+  scale_color_ochre(palette = "nolan_ned") + 
+  scale_x_continuous(breaks = df_overlaps$top_HVGs, trans = "log10") + 
   labs(x = "top HVGs", 
        y = "proportion overlapping") + 
   ggtitle("mOB: Overlap with top HVGs") + 
@@ -109,6 +109,69 @@ ggsave(fn, width = 6, height = 4)
 # ----------------------
 # calculate correlations
 # ----------------------
+
+# correlations are calculated as Spearman rank correlations between ranks of 
+# genes within the top n ranks from both method 1 (e.g. HVGs) and method 2 
+# (e.g. nnSVG); note this is usually less than n genes in total
+
+correlations <- c(100, 500, 1000)
+cor_HVGs_nnSVG <- rep(NA, length(correlations))
+cor_HVGs_nnSVG_clusters <- rep(NA, length(correlations))
+cor_nnSVG_nnSVG_clusters <- rep(NA, length(correlations))
+
+# subset and calculate correlations for each combination of methods
+for (k in seq_along(correlations)) {
+  res_sub <- 
+    filter(res_mOB, rank_HVGs <= correlations[k] & rank_nnSVG <= correlations[k])
+  cor_HVGs_nnSVG[k] <- 
+    cor(res_sub$rank_HVGs, res_sub$rank_nnSVG, method = "spearman")
+  
+  res_sub <- 
+    filter(res_mOB, rank_HVGs <= correlations[k] & rank_nnSVG_clusters <= correlations[k])
+  cor_HVGs_nnSVG_clusters[k] <- 
+    cor(res_sub$rank_HVGs, res_sub$rank_nnSVG_clusters, method = "spearman")
+  
+  res_sub <- 
+    filter(res_mOB, rank_nnSVG <= correlations[k] & rank_nnSVG_clusters <= correlations[k])
+  cor_nnSVG_nnSVG_clusters[k] <- 
+    cor(res_sub$rank_nnSVG, res_sub$rank_nnSVG_clusters, method = "spearman")
+}
+
+# summary data frame
+df_correlations <- data.frame(
+  top_n = correlations, 
+  HVGs_nnSVG = cor_HVGs_nnSVG, 
+  HVGs_nnSVGclusters = cor_HVGs_nnSVG_clusters, 
+  nnSVG_nnSVGclusters = cor_nnSVG_nnSVG_clusters
+)
+
+df_correlations <- pivot_longer(
+  df_correlations, 
+  cols = colnames(df_correlations)[-1], 
+  names_to = "methods", 
+  values_to = "correlation"
+)
+
+# plot
+ggplot(df_correlations, aes(x = top_n, y = correlation, 
+                            group = methods, color = methods)) + 
+  geom_line() + 
+  geom_point() + 
+  scale_color_ochre(palette = "nolan_ned") + 
+  scale_x_continuous(breaks = df_correlations$top_n) + 
+  labs(x = "genes within top n genes from both methods", 
+       y = "correlation between ranks") + 
+  ggtitle("mOB: Correlations between top genes") + 
+  theme_bw() + 
+  theme(panel.grid.minor.x = element_blank())
+
+fn <- here("plots", "correlations", "correlations_mOB.pdf")
+ggsave(fn, width = 6, height = 4)
+
+
+# ----------
+# plot ranks
+# ----------
 
 ggplot(res_mOB, aes(x = rank_HVGs, y = rank_nnSVG)) + 
   geom_point() + 
