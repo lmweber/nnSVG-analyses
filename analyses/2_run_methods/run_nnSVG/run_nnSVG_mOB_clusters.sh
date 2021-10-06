565848db4426fc104312426154b9cd491b663cc6
Lukas Weber <lukas.weber.edu@gmail.com>
2021-10-05 20:38:33 -0400

    record memory usage

--- analyses/2_run_methods/run_nnSVG/run_nnSVG_mOB_clusters.sh
+++ analyses/2_run_methods/run_nnSVG/run_nnSVG_mOB_clusters.sh
@@ -4,5 +4,5 @@
 #$ -l mem_free=3G,h_vmem=4G,h_fsize=100G
 
 module load conda_R/4.1.x
-Rscript run_nnSVG_mOB_clusters.R
+/usr/bin/time -v Rscript run_nnSVG_mOB_clusters.R &> ../../../outputs/memory/nnSVG_mOB_clusters.mem
 
