2c3583cd0d590132a721b544cdc635564307dcfc
Lukas Weber <lukas.weber.edu@gmail.com>
2021-10-05 21:46:52 -0400

    redirect only stderr

--- analyses/2_run_methods/run_nnSVG/run_nnSVG_mOB.sh
+++ analyses/2_run_methods/run_nnSVG/run_nnSVG_mOB.sh
@@ -4,5 +4,5 @@
 #$ -l mem_free=3G,h_vmem=4G,h_fsize=100G
 
 module load conda_R/4.1.x
-/usr/bin/time -v Rscript run_nnSVG_mOB.R &> ../../../outputs/memory/nnSVG_mOB.mem
+/usr/bin/time -v Rscript run_nnSVG_mOB.R 2> ../../../outputs/memory/nnSVG_mOB.mem
 
--- analyses/2_run_methods/run_nnSVG/run_nnSVG_mOB_clusters.sh
+++ analyses/2_run_methods/run_nnSVG/run_nnSVG_mOB_clusters.sh
@@ -4,5 +4,5 @@
 #$ -l mem_free=3G,h_vmem=4G,h_fsize=100G
 
 module load conda_R/4.1.x
-/usr/bin/time -v Rscript run_nnSVG_mOB_clusters.R &> ../../../outputs/memory/nnSVG_mOB_clusters.mem
+/usr/bin/time -v Rscript run_nnSVG_mOB_clusters.R 2> ../../../outputs/memory/nnSVG_mOB_clusters.mem
 
