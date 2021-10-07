9fccb7bfc2f69b70a66062ab0ed271a230d376a9
Lukas Weber <lukas.weber.edu@gmail.com>
2021-10-07 17:38:25 -0400

    increase memory

--- analyses/2_run_methods/run_HVGs/run_DLPFC_HVGs.sh
+++ analyses/2_run_methods/run_HVGs/run_DLPFC_HVGs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=3G,h_vmem=4G,h_fsize=100G
+#$ -l mem_free=10G,h_vmem=20G,h_fsize=100G
 
 module load conda_R/4.1.x
 /usr/bin/time -v Rscript run_DLPFC_HVGs.R 2> ../../../outputs/memory/HVGs/DLPFC_HVGs.mem
