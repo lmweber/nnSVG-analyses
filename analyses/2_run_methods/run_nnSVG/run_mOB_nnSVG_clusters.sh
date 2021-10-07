01a62f08559d71063efa2a0cf153b89a43f6c1ed
Lukas Weber <lukas.weber.edu@gmail.com>
2021-10-07 16:36:51 -0400

    fix path

--- analyses/2_run_methods/run_nnSVG/run_mOB_nnSVG_clusters.sh
+++ analyses/2_run_methods/run_nnSVG/run_mOB_nnSVG_clusters.sh
@@ -4,5 +4,5 @@
 #$ -l mem_free=3G,h_vmem=4G,h_fsize=100G
 
 module load conda_R/4.1.x
-/usr/bin/time -v Rscript run_mOB_nnSVG_clusters.R 2> ../../../outputs/memory/mOB_nnSVG/nnSVG_clusters.mem
+/usr/bin/time -v Rscript run_mOB_nnSVG_clusters.R 2> ../../../outputs/memory/nnSVG/mOB_nnSVG_clusters.mem
 
