8e2acb38361c06f8a5d5c85c32d88d7f986067e5
Lukas Weber <lukas.weber.edu@gmail.com>
2021-10-07 11:58:06 -0400

    deviance feature selection - mOB dataset

--- analyses/2_run_methods/run_HVGs/run_HVGs_mOB.sh
+++ analyses/2_run_methods/run_HVGs/run_HVGs_mOB.sh
@@ -3,5 +3,5 @@
 #$ -l mem_free=3G,h_vmem=4G,h_fsize=100G
 
 module load conda_R/4.1.x
-Rscript run_HVGs_mOB.R
+/usr/bin/time -v Rscript run_HVGs_mOB.R 2> ../../../outputs/memory/HVGs/HVGs_mOB.mem
 
---
+++ analyses/2_run_methods/run_deviance/run_deviance_clusters_mOB.R
@@ -0,0 +1,63 @@
+#######################
+# Script to run method
+# Lukas Weber, Oct 2021
+#######################
+
+# method: deviance (scry) (with covariates for clusters)
+# dataset: Spatial Transcriptomics (ST) mouse olfactory bulb (mOB)
+
+
+library(scry)
+library(SpatialExperiment)
+library(here)
+
+
+# ---------
+# load data
+# ---------
+
+# load data object with preprocessing from previous script
+
+file <- here("outputs", "SPE", "spe_mOB.rds")
+spe <- readRDS(file)
+
+spe
+
+
+# ----------
+# run method
+# ----------
+
+# run method and save the following:
+# - results
+# - runtime
+# - peak memory usage
+
+# skip filtering since this was performed during preprocessing
+
+
+# factor of cluster labels
+X <- colData(spe)$label
+
+# run deviance feature selection
+runtime <- system.time({
+  spe <- devianceFeatureSelection(spe, assay = "counts", fam = "binomial", 
+                                  batch = X)
+})
+
+# calculate ranks
+rowData(spe)$rank <- rank(-1 * rowData(spe)$binomial_deviance, ties.method = "first")
+
+# store runtime in object
+metadata(spe) <- list(
+  runtime = runtime[["elapsed"]]
+)
+
+
+# -----------
+# save object
+# -----------
+
+file <- here("outputs", "results", "deviance", "spe_deviance_clusters_mOB.rds")
+saveRDS(spe, file = file)
+
