8cdf66e5fef162292f3df6c4e736969cf9311541
Lukas Weber <lukas.weber.edu@gmail.com>
2021-10-07 15:42:38 -0400

    run deviance feature selection for DLFPC dataset

---
+++ analyses/2_run_methods/run_deviance/run_deviance_DLPFC.R
@@ -0,0 +1,58 @@
+#######################
+# Script to run method
+# Lukas Weber, Oct 2021
+#######################
+
+# method: deviance (scry)
+# dataset: 10x Genomics Visium human dorsolateral prefrontal cortex (DLPFC)
+
+
+library(scry)
+library(SpatialExperiment)
+library(here)
+
+
+# ---------
+# load data
+# ---------
+
+# load data object with preprocessing from previous script
+
+file <- here("outputs", "SPE", "spe_DLPFC.rds")
+spe <- readRDS(file)
+
+spe
+
+
+# ----------
+# run method
+# ----------
+
+# run method and save the following:
+# - results
+# - runtime
+# - peak memory usage
+
+# skip filtering since this was performed during preprocessing
+
+# run deviance feature selection
+runtime <- system.time({
+  spe <- devianceFeatureSelection(spe, assay = "counts", fam = "binomial")
+})
+
+# calculate ranks
+rowData(spe)$rank <- rank(-1 * rowData(spe)$binomial_deviance, ties.method = "first")
+
+# store runtime in object
+metadata(spe) <- list(
+  runtime = runtime[["elapsed"]]
+)
+
+
+# -----------
+# save object
+# -----------
+
+file <- here("outputs", "results", "deviance", "spe_deviance_DLPFC.rds")
+saveRDS(spe, file = file)
+
