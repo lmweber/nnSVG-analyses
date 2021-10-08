9d9aa2432b3bb11375eef0abca0916384274c59c
Lukas Weber <lukas.weber.edu@gmail.com>
2021-10-08 13:07:02 -0400

    memory

--- analyses/2_run_methods/run_nnSVG/run_SlideSeqHippo_nnSVG.sh
+++ analyses/2_run_methods/run_nnSVG/run_SlideSeqHippo_nnSVG.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -cwd
 #$ -pe local 10
-#$ -l mem_free=12G,h_vmem=15G,h_fsize=100G
+#$ -l mem_free=4G,h_vmem=5G,h_fsize=100G
 
 module load conda_R/4.1.x
 /usr/bin/time -v Rscript run_SlideSeqHippo_nnSVG.R 2> ../../../outputs/memory/nnSVG/SlideSeqHippo_nnSVG.mem
