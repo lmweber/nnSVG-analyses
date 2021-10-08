e956c209ba7683ca42bf5bb750c575e40cd9e596
Lukas Weber <lukas.weber.edu@gmail.com>
2021-10-08 13:33:22 -0400

    increase memory

--- analyses/2_run_methods/run_nnSVG/run_SlideSeqHippo_nnSVG_clusters_logcounts.sh
+++ analyses/2_run_methods/run_nnSVG/run_SlideSeqHippo_nnSVG_clusters_logcounts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -cwd
 #$ -pe local 10
-#$ -l mem_free=4G,h_vmem=5G,h_fsize=100G
+#$ -l mem_free=10G,h_vmem=12G,h_fsize=300G
 
 module load conda_R/4.1.x
 /usr/bin/time -v Rscript run_SlideSeqHippo_nnSVG_clusters_logcounts.R 2> ../../../outputs/memory/nnSVG/SlideSeqHippo_nnSVG_clusters_logcounts.mem
