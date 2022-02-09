64a5044ecb355a2e4dbc073489cf18823a4345ed
Lukas Weber <lukas.weber.edu@gmail.com>
2022-02-09 16:49:58 -0500

    scalability simulation scripts for single gene

---
+++ analyses/4_scalability_sim/scalability_nnSVG_mouseHPC_singlegene.R
@@ -0,0 +1,121 @@
+####################################
+# Script for scalability simulations
+# Lukas Weber, Feb 2022
+####################################
+
+# nnSVG, Slide-seqV2 mouse hippocampus dataset, single gene
+
+
+library(SpatialExperiment)
+library(STexampleData)
+library(nnSVG)
+library(scry)
+library(here)
+
+
+# ---------
+# load data
+# ---------
+
+spe <- SlideSeqV2_mouseHPC()
+
+dim(spe)
+
+
+# -----------
+# subsampling
+# -----------
+
+n_all <- ncol(spe)
+n_all
+
+n <- c(100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, n_all)
+
+ix <- as.list(rep(NA, length(n)))
+
+for (i in seq_along(n)) {
+  # seed for reproducibility
+  set.seed(123)
+  ix[[i]] <- sample(seq_len(n_all), n[i], replace = FALSE)
+}
+
+
+# -------------
+# preprocessing
+# -------------
+
+# starting from spots over tissue
+
+# filter low-expressed genes
+filter_genes <- 1
+n_spots <- ceiling(filter_genes / 100 * ncol(spe))
+ix_remove <- rowSums(counts(spe) > 0) < n_spots
+table(ix_remove)
+message("removing ", sum(ix_remove), " out of ", nrow(spe), " genes due to low expression")
+spe <- spe[!ix_remove, ]
+dim(spe)
+
+# filter mitochondrial genes
+is_mito <- grepl("(^MT-)|(^mt-)", rowData(spe)$gene_name)
+table(is_mito)
+message("removing ", sum(is_mito), " mitochondrial genes")
+spe <- spe[!is_mito, ]
+dim(spe)
+
+# set seed for reproducibility
+set.seed(123)
+
+# calculate deviance residuals using scry package
+spe <- nullResiduals(spe, assay = "counts", fam = "binomial", type = "deviance")
+assayNames(spe)
+
+
+# ------------------
+# select single gene
+# ------------------
+
+ix_gene <- which(rowData(spe)$gene_name == "Rgs14")
+ix_gene
+
+spe <- spe[ix_gene, ]
+dim(spe)
+
+
+# -----------------
+# run nnSVG in loop
+# -----------------
+
+# run nnSVG in loop for each subsampled set of spots
+
+res <- as.list(rep(NA, length(n)))
+runtimes <- as.list(rep(NA, length(n)))
+
+for (i in seq_along(n)) {
+  print(paste0("loop iteration i = ", i, ", n[i] = ", n[i]))
+  
+  spe_sub <- spe[, ix[[i]]]
+  
+  # seed for reproducibility
+  set.seed(123)
+  # skip filtering since already performed above
+  runtime <- system.time({
+    out <- nnSVG(spe_sub, filter_genes = FALSE, filter_mito = FALSE, n_threads = 1)
+  })
+  
+  res[[i]] <- rowData(out)
+  
+  # 'elapsed' time is real human time
+  runtimes[[i]] <- runtime[["elapsed"]]
+}
+
+
+# ------------
+# save results
+# ------------
+
+file_res <- here("outputs", "scalability", "res_scalability_nnSVG_mouseHPC_singlegene.rds")
+saveRDS(res, file = file_res)
+
+file_runtimes <- here("outputs", "scalability", "runtimes_scalability_nnSVG_mouseHPC_singlegene.rds")
+saveRDS(runtimes, file = file_runtimes)
+
