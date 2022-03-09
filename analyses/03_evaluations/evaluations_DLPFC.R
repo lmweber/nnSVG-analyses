2bc8cd2cd28b01a0541685a6aa35699c7dce1f51
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-08 23:00:02 -0500

    use updated SPE objects and update dataset name

--- analyses/03_evaluations/evaluations_DLPFC.R
+++ analyses/03_evaluations/evaluations_DLPFC.R
@@ -20,34 +20,34 @@ library(viridis)
 # scalable methods: nnSVG, SPARK-X, HVGs
 
 res_list <- list(
-  DLPFC_nnSVG = rowData(readRDS(here("outputs", "results", "nnSVG", "spe_DLPFC_nnSVG.rds"))), 
-  DLPFC_SPARKX = rowData(readRDS(here("outputs", "results", "SPARKX", "spe_DLPFC_SPARKX.rds"))), 
-  DLPFC_HVGs = rowData(readRDS(here("outputs", "results", "HVGs", "spe_DLPFC_HVGs.rds")))
+  humanDLPFC_nnSVG = rowData(readRDS(here("outputs", "results", "nnSVG", "spe_humanDLPFC_nnSVG.rds"))), 
+  humanDLPFC_SPARKX = rowData(readRDS(here("outputs", "results", "SPARKX", "spe_humanDLPFC_SPARKX.rds"))), 
+  humanDLPFC_HVGs = rowData(readRDS(here("outputs", "results", "HVGs", "spe_humanDLPFC_HVGs.rds")))
 )
 
 # add method names to all columns except gene IDs and gene names
-colnames(res_list[["DLPFC_nnSVG"]])[-(1:2)] <- paste0(colnames(res_list[["DLPFC_nnSVG"]]), "_nnSVG")[-(1:2)]
-colnames(res_list[["DLPFC_SPARKX"]])[-(1:2)] <- paste0(colnames(res_list[["DLPFC_SPARKX"]]), "_SPARKX")[-(1:2)]
-colnames(res_list[["DLPFC_HVGs"]])[-(1:2)] <- paste0(colnames(res_list[["DLPFC_HVGs"]]), "_HVGs")[-(1:2)]
+colnames(res_list[["humanDLPFC_nnSVG"]])[-(1:2)] <- paste0(colnames(res_list[["humanDLPFC_nnSVG"]]), "_nnSVG")[-(1:2)]
+colnames(res_list[["humanDLPFC_SPARKX"]])[-(1:2)] <- paste0(colnames(res_list[["humanDLPFC_SPARKX"]]), "_SPARKX")[-(1:2)]
+colnames(res_list[["humanDLPFC_HVGs"]])[-(1:2)] <- paste0(colnames(res_list[["humanDLPFC_HVGs"]]), "_HVGs")[-(1:2)]
 
 
-# ---------------------------
-# known SVGs in DLPFC dataset
-# ---------------------------
+# ---------------------------------
+# known SVGs in human DLPFC dataset
+# ---------------------------------
 
 # known SVGs: MOBP, PCP4, SNAP25, HBB, IGKC, NPY
 
 known_genes <- c("MOBP", "PCP4", "SNAP25", "HBB", "IGKC", "NPY")
 
-all(res_list$DLPFC_nnSVG$gene_id == res_list$DLPFC_SPARKX$gene_id)
-all(res_list$DLPFC_nnSVG$gene_id == res_list$DLPFC_HVGs$gene_id)
+all(res_list$humanDLPFC_nnSVG$gene_id == res_list$humanDLPFC_SPARKX$gene_id)
+all(res_list$humanDLPFC_nnSVG$gene_id == res_list$humanDLPFC_HVGs$gene_id)
 
-df_known_DLPFC <- 
-  full_join(as.data.frame(res_list$DLPFC_nnSVG), 
-            as.data.frame(res_list$DLPFC_SPARKX), 
+df_known_humanDLPFC <- 
+  full_join(as.data.frame(res_list$humanDLPFC_nnSVG), 
+            as.data.frame(res_list$humanDLPFC_SPARKX), 
             by = c("gene_id", "gene_name")) %>% 
   full_join(., 
-            as.data.frame(res_list$DLPFC_HVGs), 
+            as.data.frame(res_list$humanDLPFC_HVGs), 
             by = c("gene_id", "gene_name")) %>% 
   filter(gene_name %in% known_genes) %>% 
   pivot_longer(c("rank_nnSVG", "rank_SPARKX", "rank_HVGs"), 
@@ -59,22 +59,22 @@ df_known_DLPFC <-
 
 
 # plot ranks
-ggplot(as.data.frame(df_known_DLPFC), 
+ggplot(as.data.frame(df_known_humanDLPFC), 
        aes(x = gene_name, y = rank, group = method, color = method, 
            shape = method, label = rank)) + 
   geom_point(stroke = 1.5, size = 1.75) + 
   scale_shape_manual(values = c(4, 3, 1)) + 
   scale_color_manual(values = c("blue3", "maroon", "darkorange")) + 
-  scale_y_log10(limits = c(6, 22000)) + 
+  scale_y_log10(limits = c(3, 5000)) + 
   geom_vline(xintercept = 3.5, linetype = "dashed", color = "gray50") + 
   geom_text_repel(nudge_x = 0.3, size = 1.75, segment.color = NA, show.legend = FALSE) + 
-  annotate("text", label = "large bandwidth", x = 2, y = 22000, size = 4) + 
-  annotate("text", label = "small bandwidth", x = 5, y = 22000, size = 4) + 
+  annotate("text", label = "large bandwidth", x = 2, y = 5000, size = 4) + 
+  annotate("text", label = "small bandwidth", x = 5, y = 5000, size = 4) + 
   labs(x = "gene", y = "rank") + 
-  ggtitle("Example SVGs: DLPFC") + 
+  ggtitle("Example SVGs: human DLPFC") + 
   theme_bw()
 
-fn <- here(file.path("plots", "evaluations", "known_genes_ranks_DLPFC"))
+fn <- here(file.path("plots", "evaluations", "known_genes_ranks_humanDLPFC"))
 ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
 ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
@@ -103,15 +103,15 @@ length(manual_genes_all)
 
 # plot likelihood ratio statistics vs. ranks
 
-df_nnSVG_DLPFC <- 
-  as.data.frame(res_list$DLPFC_nnSVG) %>% 
+df_nnSVG_humanDLPFC <- 
+  as.data.frame(res_list$humanDLPFC_nnSVG) %>% 
   mutate(is_known = gene_name %in% known_genes) %>% 
   mutate(is_marker = gene_name %in% manual_gene_names) %>% 
   mutate(is_marker_or_known = is_marker | is_known)
 
 # rank at p-value = 0.05 cutoff
 padj_cutoff_nnSVG <- 
-  as.data.frame(res_list$DLPFC_nnSVG) %>% 
+  as.data.frame(res_list$humanDLPFC_nnSVG) %>% 
   filter(padj_nnSVG <= 0.05) %>% 
   summarize(max = max(rank_nnSVG)) %>% 
   unlist
@@ -119,25 +119,25 @@ padj_cutoff_nnSVG <-
 padj_cutoff_nnSVG
 
 # number of 190 marker genes identified as significant
-table(filter(df_nnSVG_DLPFC, is_marker)$padj_nnSVG <= 0.05)
+table(filter(df_nnSVG_humanDLPFC, is_marker)$padj_nnSVG <= 0.05)
 # number of combined set identified as significant
-table(filter(df_nnSVG_DLPFC, is_marker_or_known)$padj_nnSVG <= 0.05)
+table(filter(df_nnSVG_humanDLPFC, is_marker_or_known)$padj_nnSVG <= 0.05)
 
 
 # highlighting 6 known genes
-ggplot(as.data.frame(df_nnSVG_DLPFC), 
+ggplot(as.data.frame(df_nnSVG_humanDLPFC), 
        aes(x = rank_nnSVG, y = LR_stat_nnSVG, label = gene_name)) + 
   geom_line(color = "navy") + 
-  geom_point(data = filter(df_nnSVG_DLPFC, gene_name %in% known_genes), 
+  geom_point(data = filter(df_nnSVG_humanDLPFC, gene_name %in% known_genes), 
              size = 2, color = "firebrick3") + 
-  geom_text_repel(data = filter(df_nnSVG_DLPFC, gene_name %in% known_genes), 
+  geom_text_repel(data = filter(df_nnSVG_humanDLPFC, gene_name %in% known_genes), 
                   nudge_x = 80, nudge_y = 350, size = 3, color = "firebrick3") + 
   xlim(c(0, 1000)) + 
   labs(x = "rank", y = "likelihood ratio statistic") + 
-  ggtitle("nnSVG: DLPFC, example SVGs") + 
+  ggtitle("nnSVG: human DLPFC, example SVGs") + 
   theme_bw()
 
-fn <- here(file.path("plots", "evaluations", "LR_stat_ranks_6known_DLPFC"))
+fn <- here(file.path("plots", "evaluations", "LR_stat_ranks_6known_humanDLPFC"))
 ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
 ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
@@ -145,26 +145,26 @@ ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 # highlighting 6 known genes and 193 combined set of known and layer-specific markers
 # set seed for overlapping geom_text_repel
 set.seed(1)
-ggplot(as.data.frame(df_nnSVG_DLPFC), 
+ggplot(as.data.frame(df_nnSVG_humanDLPFC), 
        aes(x = rank_nnSVG, y = LR_stat_nnSVG, label = gene_name)) + 
   geom_line(color = "navy") + 
   ylim(c(-2, 8700)) + 
-  geom_point(data = filter(df_nnSVG_DLPFC, gene_name %in% manual_genes_all), 
+  geom_point(data = filter(df_nnSVG_humanDLPFC, gene_name %in% manual_genes_all), 
              pch = 1, size = 2, color = "firebrick3") + 
-  geom_point(data = filter(df_nnSVG_DLPFC, gene_name %in% known_genes), 
+  geom_point(data = filter(df_nnSVG_humanDLPFC, gene_name %in% known_genes), 
              pch = 1, size = 2, stroke = 0.75, color = "black") + 
-  geom_text_repel(data = filter(df_nnSVG_DLPFC, gene_name %in% known_genes), 
-                  nudge_x = 1600, nudge_y = 800, size = 3, 
+  geom_text_repel(data = filter(df_nnSVG_humanDLPFC, gene_name %in% known_genes), 
+                  nudge_x = 400, nudge_y = 800, size = 3, 
                   segment.color = "black", color = "firebrick3") + 
   geom_vline(xintercept = padj_cutoff_nnSVG, 
              linetype = "dashed", color = "darkorange2") + 
   annotate("text", label = paste0("adjusted p-value = 0.05\n(rank ", padj_cutoff_nnSVG, ")"), 
-           x = 6750, y = 7000, size = 4, color = "darkorange2") + 
+           x = 1500, y = 7500, size = 4, color = "darkorange2") + 
   labs(x = "rank", y = "likelihood ratio statistic") + 
-  ggtitle("nnSVG: DLPFC, example SVGs and markers") + 
+  ggtitle("nnSVG: human DLPFC, example SVGs and markers") + 
   theme_bw()
 
-fn <- here(file.path("plots", "evaluations", "LR_stat_ranks_193knownAndMarkers_nnSVG_DLPFC"))
+fn <- here(file.path("plots", "evaluations", "LR_stat_ranks_193knownAndMarkers_nnSVG_humanDLPFC"))
 ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
 ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
@@ -175,15 +175,15 @@ ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
 # plot adjusted p-values vs. ranks
 
-df_SPARKX_DLPFC <- 
-  as.data.frame(res_list$DLPFC_SPARKX) %>% 
+df_SPARKX_humanDLPFC <- 
+  as.data.frame(res_list$humanDLPFC_SPARKX) %>% 
   mutate(is_known = gene_name %in% known_genes) %>% 
   mutate(is_marker = gene_name %in% manual_gene_names) %>% 
   mutate(is_marker_or_known = is_marker | is_known)
 
 # rank at p-value = 0.05 cutoff
 padj_cutoff_SPARKX <- 
-  as.data.frame(res_list$DLPFC_SPARKX) %>% 
+  as.data.frame(res_list$humanDLPFC_SPARKX) %>% 
   filter(adjustedPval_SPARKX <= 0.05) %>% 
   summarize(max = max(rank_SPARKX)) %>% 
   unlist
@@ -191,31 +191,31 @@ padj_cutoff_SPARKX <-
 padj_cutoff_SPARKX
 
 # number of 190 marker genes identified as significant
-table(filter(df_SPARKX_DLPFC, is_marker)$adjustedPval_SPARKX <= 0.05)
+table(filter(df_SPARKX_humanDLPFC, is_marker)$adjustedPval_SPARKX <= 0.05)
 # number of combined set identified as significant
-table(filter(df_SPARKX_DLPFC, is_marker_or_known)$adjustedPval_SPARKX <= 0.05)
+table(filter(df_SPARKX_humanDLPFC, is_marker_or_known)$adjustedPval_SPARKX <= 0.05)
 
 
 # highlighting 193 (combined set of known and layer-specific marker)
-ggplot(as.data.frame(df_SPARKX_DLPFC), 
+ggplot(as.data.frame(df_SPARKX_humanDLPFC), 
        aes(x = rank_SPARKX, y = -log10(adjustedPval_SPARKX), label = gene_name)) + 
   geom_line(color = "navy") + 
-  geom_point(data = filter(df_SPARKX_DLPFC, gene_name %in% manual_genes_all), 
+  geom_point(data = filter(df_SPARKX_humanDLPFC, gene_name %in% manual_genes_all), 
              pch = 1, size = 2, color = "firebrick3") + 
-  geom_point(data = filter(df_SPARKX_DLPFC, gene_name %in% known_genes), 
+  geom_point(data = filter(df_SPARKX_humanDLPFC, gene_name %in% known_genes), 
              pch = 1, size = 2, stroke = 0.75, color = "black") + 
-  geom_text_repel(data = filter(df_SPARKX_DLPFC, gene_name %in% known_genes), 
-                  nudge_x = 1500, nudge_y = 30, size = 3, 
+  geom_text_repel(data = filter(df_SPARKX_humanDLPFC, gene_name %in% known_genes), 
+                  nudge_x = 500, nudge_y = 30, size = 3, 
                   segment.color = "black", color = "firebrick3") + 
   geom_vline(xintercept = padj_cutoff_SPARKX, 
              linetype = "dashed", color = "darkorange2") + 
   annotate("text", label = paste0("adjusted p-value\n = 0.05\n(rank ", padj_cutoff_SPARKX, ")"), 
-           x = 12500, y = 225, size = 4, color = "darkorange2") + 
+           x = 2600, y = 250, size = 4, color = "darkorange2") + 
   labs(x = "rank", y = "-log10(adjusted p-value)") + 
-  ggtitle("SPARK-X: DLPFC, example SVGs and markers") + 
+  ggtitle("SPARK-X: human DLPFC, example SVGs and markers") + 
   theme_bw()
 
-fn <- here(file.path("plots", "evaluations", "adjPvals_ranks_193knownAndMarkers_SPARKX_DLPFC"))
+fn <- here(file.path("plots", "evaluations", "adjPvals_ranks_193knownAndMarkers_SPARKX_humanDLPFC"))
 ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
 ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
@@ -227,7 +227,7 @@ ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 # LR statistic vs. effect size
 
 df_effect <- 
-  as.data.frame(df_nnSVG_DLPFC) %>% 
+  as.data.frame(df_nnSVG_humanDLPFC) %>% 
   mutate(is_marker_or_known = is_marker | is_known) %>% 
   mutate(l_nnSVG = 1 / phi_nnSVG) %>% 
   filter(rank_nnSVG <= 1000)
@@ -248,10 +248,10 @@ ggplot(df_effect,
   labs(x = "mean logcounts", 
        y = "proportion spatial variance", 
        color = "LR statistic") + 
-  ggtitle("nnSVG: DLPFC") + 
+  ggtitle("nnSVG: human DLPFC") + 
   theme_bw()
 
-fn <- here(file.path("plots", "evaluations", "effect_size_mean_DLPFC"))
+fn <- here(file.path("plots", "evaluations", "effect_size_mean_humanDLPFC"))
 ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
 ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
@@ -292,15 +292,15 @@ calc_overlaps <- function(method1, method2) {
 
 # use function to calculate overlaps
 
-df_overlaps_DLPFC <- data.frame(
+df_overlaps_humanDLPFC <- data.frame(
   top_n = overlaps, 
-  dataset = "DLPFC", 
-  nnSVG = calc_overlaps("DLPFC_HVGs", "DLPFC_nnSVG"), 
-  SPARKX = calc_overlaps("DLPFC_HVGs", "DLPFC_SPARKX")
+  dataset = "humanDLPFC", 
+  nnSVG = calc_overlaps("humanDLPFC_HVGs", "humanDLPFC_nnSVG"), 
+  SPARKX = calc_overlaps("humanDLPFC_HVGs", "humanDLPFC_SPARKX")
 )
 
 df_overlaps <- 
-  pivot_longer(df_overlaps_DLPFC, cols = c("nnSVG", "SPARKX"), 
+  pivot_longer(df_overlaps_humanDLPFC, cols = c("nnSVG", "SPARKX"), 
                names_to = "method", values_to = "proportion")
 
 
@@ -314,11 +314,11 @@ ggplot(as.data.frame(df_overlaps),
   ylim(c(0, 1)) + 
   xlab("top n genes") + 
   ylab("proportion overlap") + 
-  ggtitle("Overlap SVGs and HVGs: DLPFC") + 
+  ggtitle("Overlap SVGs and HVGs: human DLPFC") + 
   theme_bw() + 
   theme(panel.grid.minor = element_blank())
 
-fn <- here(file.path("plots", "evaluations", "prop_overlap_DLPFC"))
+fn <- here(file.path("plots", "evaluations", "prop_overlap_humanDLPFC"))
 ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
 ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
@@ -329,9 +329,9 @@ ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
 # top 1000 ranked genes from each method
 
-df_ranks_DLPFC_nnSVG_HVGs <- 
-  full_join(as.data.frame(res_list$DLPFC_nnSVG), 
-            as.data.frame(res_list$DLPFC_HVGs), 
+df_ranks_humanDLPFC_nnSVG_HVGs <- 
+  full_join(as.data.frame(res_list$humanDLPFC_nnSVG), 
+            as.data.frame(res_list$humanDLPFC_HVGs), 
             by = c("gene_id", "gene_name")) %>% 
   mutate(rank_method = rank_nnSVG) %>% 
   mutate(method = "nnSVG") %>% 
@@ -339,9 +339,9 @@ df_ranks_DLPFC_nnSVG_HVGs <-
   filter(rank_HVGs <= 1000) %>% 
   select(c("gene_id", "gene_name", "rank_HVGs", "rank_method", "method"))
 
-df_ranks_DLPFC_SPARKX_HVGs <- 
-  full_join(as.data.frame(res_list$DLPFC_SPARKX), 
-            as.data.frame(res_list$DLPFC_HVGs), 
+df_ranks_humanDLPFC_SPARKX_HVGs <- 
+  full_join(as.data.frame(res_list$humanDLPFC_SPARKX), 
+            as.data.frame(res_list$humanDLPFC_HVGs), 
             by = c("gene_id", "gene_name")) %>% 
   mutate(rank_method = rank_SPARKX) %>% 
   mutate(method = "SPARKX") %>% 
@@ -349,14 +349,14 @@ df_ranks_DLPFC_SPARKX_HVGs <-
   filter(rank_HVGs <= 1000) %>% 
   select(c("gene_id", "gene_name", "rank_HVGs", "rank_method", "method"))
 
-df_ranks_DLPFC <- full_join(df_ranks_DLPFC_nnSVG_HVGs, df_ranks_DLPFC_SPARKX_HVGs)
+df_ranks_humanDLPFC <- full_join(df_ranks_humanDLPFC_nnSVG_HVGs, df_ranks_humanDLPFC_SPARKX_HVGs)
 
 
 # calculate Spearman correlations
-cor_nnSVG <- cor(df_ranks_DLPFC_nnSVG_HVGs$rank_method, 
-                 df_ranks_DLPFC_nnSVG_HVGs$rank_HVGs, method = "spearman")
-cor_SPARKX <- cor(df_ranks_DLPFC_SPARKX_HVGs$rank_method, 
-                  df_ranks_DLPFC_SPARKX_HVGs$rank_HVGs, method = "spearman")
+cor_nnSVG <- cor(df_ranks_humanDLPFC_nnSVG_HVGs$rank_method, 
+                 df_ranks_humanDLPFC_nnSVG_HVGs$rank_HVGs, method = "spearman")
+cor_SPARKX <- cor(df_ranks_humanDLPFC_SPARKX_HVGs$rank_method, 
+                  df_ranks_humanDLPFC_SPARKX_HVGs$rank_HVGs, method = "spearman")
 
 ann_text <- data.frame(
   x = 800, 
@@ -367,7 +367,7 @@ ann_text <- data.frame(
 
 
 # plot comparisons of ranks
-ggplot(as.data.frame(df_ranks_DLPFC), 
+ggplot(as.data.frame(df_ranks_humanDLPFC), 
        aes(x = rank_HVGs, y = rank_method, color = method)) + 
   facet_wrap(~ method) + 
   geom_point() + 
@@ -379,10 +379,10 @@ ggplot(as.data.frame(df_ranks_DLPFC),
   ylim(c(0, 1000)) + 
   xlab("rank HVGs") + 
   ylab("rank SVGs") + 
-  ggtitle("Ranks SVGs vs. HVGs: DLPFC") + 
+  ggtitle("Ranks SVGs vs. HVGs: human DLPFC") + 
   theme_bw()
 
-fn <- here(file.path("plots", "evaluations", "ranks_scatter_DLPFC"))
+fn <- here(file.path("plots", "evaluations", "ranks_scatter_humanDLPFC"))
 ggsave(paste0(fn, ".pdf"), width = 8, height = 4)
 ggsave(paste0(fn, ".png"), width = 8, height = 4)
 
@@ -391,16 +391,16 @@ ggsave(paste0(fn, ".png"), width = 8, height = 4)
 # p-value distributions
 # ---------------------
 
-df_pvals_DLPFC <- as.data.frame(res_list$DLPFC_nnSVG)
+df_pvals_humanDLPFC <- as.data.frame(res_list$humanDLPFC_nnSVG)
 
 # plot p-values
-ggplot(as.data.frame(df_pvals_DLPFC), aes(x = pval_nnSVG)) + 
+ggplot(as.data.frame(df_pvals_humanDLPFC), aes(x = pval_nnSVG)) + 
   geom_histogram(color = "black", fill = "blue3", bins = 30) + 
   xlab("p-values") + 
-  ggtitle("P-values: nnSVG, DLPFC") + 
+  ggtitle("P-values: nnSVG, human DLPFC") + 
   theme_bw()
 
-fn <- here(file.path("plots", "evaluations", "pvals_DLPFC"))
+fn <- here(file.path("plots", "evaluations", "pvals_humanDLPFC"))
 ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
 ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
@@ -409,13 +409,13 @@ ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 # bandwidth distributions
 # -----------------------
 
-df_bandwidth_DLPFC <- 
-  as.data.frame(res_list$DLPFC_nnSVG) %>% 
+df_bandwidth_humanDLPFC <- 
+  as.data.frame(res_list$humanDLPFC_nnSVG) %>% 
   mutate(is_known = gene_name %in% known_genes) %>% 
   mutate(l_nnSVG = 1 / phi_nnSVG)
 
-phis <- df_bandwidth_DLPFC$phi_nnSVG
-names(phis) <- df_bandwidth_DLPFC$gene_name
+phis <- df_bandwidth_humanDLPFC$phi_nnSVG
+names(phis) <- df_bandwidth_humanDLPFC$gene_name
 phis_known <- phis[known_genes]
 
 ls_known <- 1 / phis_known
@@ -428,7 +428,7 @@ ann_text <- data.frame(
 )
 
 # plot bandwidth l (inverse of phi, scaled to distances 0 to 1)
-ggplot(as.data.frame(df_bandwidth_DLPFC), aes(x = l_nnSVG)) + 
+ggplot(as.data.frame(df_bandwidth_humanDLPFC), aes(x = l_nnSVG)) + 
   geom_density(color = "black", fill = "skyblue") + 
   xlim(c(0, 1)) + 
   geom_point(data = ann_text, aes(x = x, y = y), color = "red", size = 2) + 
@@ -436,10 +436,10 @@ ggplot(as.data.frame(df_bandwidth_DLPFC), aes(x = l_nnSVG)) +
                   nudge_x = 0.13, nudge_y = 1, color = "red", size = 3) + 
   xlab("bandwidth (1 / phi)") + 
   ylab("density") + 
-  ggtitle("Bandwidths: nnSVG, DLPFC") + 
+  ggtitle("Bandwidths: nnSVG, human DLPFC") + 
   theme_bw()
 
-fn <- here(file.path("plots", "evaluations", "bandwidths_DLPFC"))
+fn <- here(file.path("plots", "evaluations", "bandwidths_humanDLPFC"))
 ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
 ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
