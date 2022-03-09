7ee8dc4d60ba51c989e310de06c20dc8a9ffb2c5
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-08 22:39:03 -0500

    update script to run SPARK-X

---
+++ analyses/02_run_methods/run_SPARKX/run_SPARKX_humanDLPFC.R
@@ -0,0 +1,72 @@
+#######################
+# Script to run method
+# Lukas Weber, Mar 2022
+#######################
+
+# method: SPARK-X
+# dataset: human DLPFC
+
+
+library(SpatialExperiment)
+library(SPARK)
+library(here)
+
+
+# ---------
+# load data
+# ---------
+
+# load data object with preprocessing from previous script
+
+fn <- here("outputs", "SPE", "spe_humanDLPFC_preprocessed.rds")
+spe <- readRDS(fn)
+
+dim(spe)
+
+
+# ----------
+# run method
+# ----------
+
+# run method and save results, runtime, peak memory usage
+
+# run SPARK-X
+set.seed(123)
+runtime <- system.time({
+  sparkx_out <- sparkx(
+    count_in = counts(spe), 
+    locus_in = spatialCoords(spe), 
+    X_in = NULL, 
+    numCores = 10, 
+    option = "mixture", 
+    verbose = TRUE
+  )
+})
+
+# results for individual kernels
+head(sparkx_out$stats)
+head(sparkx_out$res_stest)
+# results for combined kernels
+head(sparkx_out$res_mtest)
+
+# store results in SPE object
+stopifnot(all(rownames(sparkx_out$res_mtest) == rowData(spe)$gene_id))
+
+rowData(spe) <- cbind(rowData(spe), sparkx_out$res_mtest)
+
+# calculate ranks
+rowData(spe)$rank <- rank(rowData(spe)$combinedPval, ties.method = "first")
+
+# store runtime in object
+metadata(spe) <- list(
+  runtime = runtime[["elapsed"]]
+)
+
+
+# -----------
+# save object
+# -----------
+
+file <- here("outputs", "results", "SPARKX", "spe_humanDLPFC_nnSVG.rds")
+saveRDS(spe, file = file)
+
