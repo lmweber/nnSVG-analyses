bb9d623225cfe20acb18b14d5da6886a594fc19a
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-08 22:47:43 -0500

    update scripts to run HVGs and deviance

---
+++ analyses/02_run_methods/run_deviance/run_deviance_humanDLPFC.R
@@ -0,0 +1,54 @@
+#######################
+# Script to run method
+# Lukas Weber, Mar 2022
+#######################
+
+# method: deviance
+# dataset: human DLPFC
+
+
+library(SpatialExperiment)
+library(scry)
+library(here)
+
+
+# ---------
+# load data
+# ---------
+
+# load data object with preprocessing from previous script
+
+fn <- here("outputs", "SPE", "spe_humanDLPFC_preprocessed.rds")
+spe <- readRDS(fn)
+
+dim(spe)
+
+
+# ----------
+# run method
+# ----------
+
+# run method and save results, runtime, peak memory usage
+
+# run deviance feature selection
+set.seed(123)
+runtime <- system.time({
+  spe <- devianceFeatureSelection(spe, assay = "counts", fam = "binomial")
+})
+
+# calculate ranks
+rowData(spe)$rank <- rank(-1 * rowData(spe)$binomial_deviance, ties.method = "first")
+
+# store runtime in object
+metadata(spe) <- list(
+  runtime = runtime[["elapsed"]]
+)
+
+
+# -----------
+# save object
+# -----------
+
+file <- here("outputs", "results", "deviance", "spe_humanDLPFC_deviance.rds")
+saveRDS(spe, file = file)
+
