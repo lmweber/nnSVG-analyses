fdef2319b3d975c724aa279823593a82ad84f0dd
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-12 13:03:19 -0500

    update shell wrappers

---
+++ analyses/02_run_methods/run_SPARKX/run_SPARKX_humanDLPFC_noFilt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=2G,h_vmem=3G,h_fsize=100G
+
+module load conda_R/4.1.x
+/usr/bin/time -v Rscript run_SPARKX_humanDLPFC_noFilt.R 2> ../../../outputs/memory/SPARKX_humanDLPFC_noFilt.mem
+
---
+++ analyses/02_run_methods/run_nnSVG/run_nnSVG_humanDLPFC_noFilt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#$ -cwd
+#$ -pe local 10
+#$ -l mem_free=2G,h_vmem=3G,h_fsize=100G
+
+module load conda_R/4.1.x
+/usr/bin/time -v Rscript run_nnSVG_humanDLPFC_noFilt.R 2> ../../../outputs/memory/nnSVG_humanDLPFC_noFilt.mem
+
