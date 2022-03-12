ab6756d65898f8a2d994824c9f353ae403032b2a
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-12 13:47:27 -0500

    memory in qsub

--- analyses/02_run_methods/run_HVGs/run_HVGs_humanDLPFC.sh
+++ analyses/02_run_methods/run_HVGs/run_HVGs_humanDLPFC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=2G,h_vmem=3G,h_fsize=100G
+#$ -l mem_free=3G,h_vmem=4G,h_fsize=100G
 
 module load conda_R/4.1.x
 /usr/bin/time -v Rscript run_HVGs_humanDLPFC.R 2> ../../../outputs/memory/HVGs_humanDLPFC.mem
