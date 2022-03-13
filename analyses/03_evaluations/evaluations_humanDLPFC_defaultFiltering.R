5e4c1364ebd5af1aec1c24a32e89240703a92c42
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-13 15:22:36 -0400

    remove duplicated plots between filtering setups

--- analyses/03_evaluations/evaluations_humanDLPFC_defaultFiltering.R
+++ analyses/03_evaluations/evaluations_humanDLPFC_defaultFiltering.R
@@ -4,7 +4,7 @@
 #################################
 
 # data set: human DLPFC
-# gene filtering: defaults for nnSVG and SPARK-X
+# filtering: default filtering of low-expressed genes for nnSVG and SPARK-X
 
 
 library(SpatialExperiment)
@@ -30,8 +30,8 @@ dir_plots <- here(file.path("plots", "evaluations", "humanDLPFC", "default_filte
 # note choice of filtering per method
 res_list <- list(
   humanDLPFC_nnSVG = rowData(readRDS(here("outputs", "results", "spe_humanDLPFC_nnSVG.rds"))), 
-  humanDLPFC_SPARKX = rowData(readRDS(here("outputs", "results", "spe_humanDLPFC_SPARKX_nofilt.rds"))), 
-  humanDLPFC_HVGs = rowData(readRDS(here("outputs", "results", "spe_humanDLPFC_HVGs_nofilt.rds")))
+  humanDLPFC_SPARKX = rowData(readRDS(here("outputs", "results", "spe_humanDLPFC_SPARKX_noFilt.rds"))), 
+  humanDLPFC_HVGs = rowData(readRDS(here("outputs", "results", "spe_humanDLPFC_HVGs_noFilt.rds")))
 )
 
 # add method names to all columns except gene IDs and gene names
@@ -93,9 +93,9 @@ ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
 ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
 
-# --------------------------------------------
-# nnSVG: likelihood ratio statistics vs. ranks
-# --------------------------------------------
+# ------------------------------------
+# SPARK-X: combined p-values vs. ranks
+# ------------------------------------
 
 # layer-specific marker genes from manually guided analyses in spatialLIBD
 # see original code at:
@@ -113,91 +113,6 @@ head(markers_names)
 markers_names_all <- unique(c(known_genes, markers_names))
 length(markers_names_all)
 
-# note: numbers of genes
-# 198 manual marker genes
-length(markers_names)
-# 195 additional manual marker genes excluding overlap with 6 known genes
-length(markers_names) - sum(known_genes %in% markers_names)
-# 201 total manual marker genes and known genes (i.e. 3 overlapping)
-length(markers_names_all)
-
-
-# plot likelihood ratio statistics vs. ranks
-
-df_nnSVG <- 
-  as.data.frame(res_list$humanDLPFC_nnSVG) %>% 
-  mutate(is_known = gene_name %in% known_genes) %>% 
-  mutate(is_marker = gene_name %in% markers_names) %>% 
-  mutate(is_marker_or_known = is_marker | is_known)
-
-# rank at adjusted p-value = 0.05 cutoff
-padj_cutoff_nnSVG <- 
-  as.data.frame(res_list$humanDLPFC_nnSVG) %>% 
-  filter(padj_nnSVG <= 0.05) %>% 
-  summarize(max = max(rank_nnSVG)) %>% 
-  unlist
-
-padj_cutoff_nnSVG
-
-# number of significant SVGs
-table(df_nnSVG$padj_nnSVG <= 0.05)
-
-# number of markers and known in nnSVG gene set (i.e. pass filtering)
-table(df_nnSVG$gene_name %in% markers_names_all)
-# number of marker genes identified as significant
-table(filter(df_nnSVG, is_marker)$padj_nnSVG <= 0.05)
-# number of combined set identified as significant
-table(filter(df_nnSVG, is_marker_or_known)$padj_nnSVG <= 0.05)
-
-
-# highlighting known genes
-ggplot(as.data.frame(df_nnSVG), 
-       aes(x = rank_nnSVG, y = LR_stat_nnSVG, label = gene_name)) + 
-  geom_line(color = "navy") + 
-  geom_point(data = filter(df_nnSVG, gene_name %in% known_genes), 
-             size = 2, color = "firebrick3") + 
-  geom_text_repel(data = filter(df_nnSVG, gene_name %in% known_genes), 
-                  nudge_x = 80, nudge_y = 350, size = 3, color = "firebrick3") + 
-  xlim(c(0, 1000)) + 
-  labs(x = "rank", y = "likelihood ratio statistic") + 
-  ggtitle("nnSVG: human DLPFC") + 
-  theme_bw()
-
-fn <- file.path(dir_plots, "stat_vs_rank_top1000_nnSVG_humanDLPFC_defaultFilt")
-ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
-ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
-
-
-# highlighting known and marker genes
-# set seed for overlapping geom_text_repel
-set.seed(1)
-ggplot(as.data.frame(df_nnSVG), 
-       aes(x = rank_nnSVG, y = LR_stat_nnSVG, label = gene_name)) + 
-  geom_line(color = "navy") + 
-  ylim(c(-1, 6200)) + 
-  geom_point(data = filter(df_nnSVG, gene_name %in% markers_names_all), 
-             pch = 1, size = 2, color = "firebrick3") + 
-  geom_point(data = filter(df_nnSVG, gene_name %in% known_genes), 
-             pch = 1, size = 2, stroke = 0.75, color = "black") + 
-  geom_text_repel(data = filter(df_nnSVG, gene_name %in% known_genes), 
-                  nudge_x = 500, nudge_y = 800, size = 3, 
-                  segment.color = "black", color = "firebrick3") + 
-  geom_vline(xintercept = padj_cutoff_nnSVG, 
-             linetype = "dashed", color = "darkorange2") + 
-  annotate("text", label = paste0("adjusted p-value = 0.05\n(rank ", padj_cutoff_nnSVG, ")"), 
-           x = 2850, y = 5000, size = 3, color = "darkorange2") + 
-  labs(x = "rank", y = "likelihood ratio statistic") + 
-  ggtitle("nnSVG: human DLPFC") + 
-  theme_bw()
-
-fn <- file.path(dir_plots, "stat_vs_rank_markers_nnSVG_humanDLPFC_defaultFilt")
-ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
-ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
-
-
-# ------------------------------------
-# SPARK-X: combined p-values vs. ranks
-# ------------------------------------
 
 # plot combined p-values vs. ranks
 
@@ -247,42 +162,6 @@ ggplot(as.data.frame(df_SPARKX),
   theme_bw()
 
 fn <- file.path(dir_plots, "stat_vs_rank_SPARKX_humanDLPFC_defaultFilt")
-ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
-ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
-
-
-# ------------
-# effect sizes
-# ------------
-
-# LR statistic vs. effect size
-
-df_effect <- 
-  as.data.frame(df_nnSVG) %>% 
-  mutate(is_marker_or_known = is_marker | is_known) %>% 
-  mutate(l_nnSVG = 1 / phi_nnSVG) %>% 
-  filter(rank_nnSVG <= 1000)
-
-
-# effect size vs. mean
-ggplot(df_effect, 
-       aes(x = mean_nnSVG, y = prop_sv_nnSVG, color = LR_stat_nnSVG)) + 
-  geom_point(size = 0.75) + 
-  scale_color_viridis(trans = "log10") + 
-  geom_point(data = df_effect %>% filter(is_known), 
-             aes(shape = is_known), color = "red", size = 0.8) + 
-  scale_shape_manual(values = 1, name = "known") + 
-  geom_text_repel(
-    data = df_effect %>% filter(is_known), 
-    aes(label = gene_name), color = "red", size = 3, nudge_x = 0.4, nudge_y = 0.05) + 
-  ylim(c(0, 1)) + 
-  labs(x = "mean logcounts", 
-       y = "proportion spatial variance", 
-       color = "LR statistic") + 
-  ggtitle("nnSVG: human DLPFC") + 
-  theme_bw()
-
-fn <- file.path(dir_plots, "effect_size_nnSVG_humanDLPFC_defaultFilt")
 ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
 ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
@@ -416,62 +295,4 @@ ggplot(as.data.frame(df_ranks),
 fn <- file.path(dir_plots, "ranks_humanDLPFC_defaultFilt")
 ggsave(paste0(fn, ".pdf"), width = 8, height = 4)
 ggsave(paste0(fn, ".png"), width = 8, height = 4)
-
-
-# ---------------------
-# p-value distributions
-# ---------------------
-
-df_pvals <- as.data.frame(res_list$humanDLPFC_nnSVG)
-
-# plot p-values
-ggplot(as.data.frame(df_pvals), aes(x = pval_nnSVG)) + 
-  geom_histogram(color = "black", fill = "blue3", bins = 30) + 
-  labs(x = "p-values", 
-       y = "frequency") + 
-  ggtitle("nnSVG p-values: human DLPFC") + 
-  theme_bw()
-
-fn <- file.path(dir_plots, "pvals_nnSVG_humanDLPFC_defaultFilt")
-ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
-ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
-
-
-# --------------------------------------
-# length scale (bandwidth) distributions
-# --------------------------------------
-
-df_bandwidth <- 
-  as.data.frame(res_list$humanDLPFC_nnSVG) %>% 
-  mutate(is_known = gene_name %in% known_genes) %>% 
-  mutate(l_nnSVG = 1 / phi_nnSVG)
-
-phis <- df_bandwidth$phi_nnSVG
-names(phis) <- df_bandwidth$gene_name
-phis_known <- phis[known_genes]
-
-ls_known <- 1 / phis_known
-ls_known
-
-ann_text <- data.frame(
-  x = unname(ls_known), 
-  y =  c(0.25, 2.25, 1.25, 6.2, 6.15, 5), 
-  label = paste0(names(ls_known), " = ", format(round(ls_known, 3), nsmall = 3))
-)
-
-# plot bandwidth l (inverse of phi, scaled to distances 0 to 1)
-ggplot(as.data.frame(df_bandwidth), aes(x = l_nnSVG)) + 
-  geom_density(color = "black", fill = "skyblue") + 
-  xlim(c(0, 1)) + 
-  geom_point(data = ann_text, aes(x = x, y = y), color = "red", size = 2) + 
-  geom_text_repel(data = ann_text, aes(x = x, y = y, label = label), 
-                  nudge_x = 0.15, nudge_y = 0.1, color = "red", size = 3) + 
-  xlab("estimated length scale") + 
-  ylab("density") + 
-  ggtitle("nnSVG length scales: human DLPFC") + 
-  theme_bw()
-
-fn <- file.path(dir_plots, "lengthscales_nnSVG_humanDLPFC_defaultFilt")
-ggsave(paste0(fn, ".pdf"), width = 5.25, height = 4)
-ggsave(paste0(fn, ".png"), width = 5.25, height = 4)
 
