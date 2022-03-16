e92a005912a931f5ba66084f24420994c965a391
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-16 14:35:44 -0400

    scripts for nnSVG and SPARK-X without covariates

---
+++ analyses/02_run_methods/run_SPARKX/run_SPARKX_mouseHPC_noCovariates.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#$ -cwd
+#$ -pe local 20
+#$ -l mem_free=2G,h_vmem=3G,h_fsize=100G
+
+module load conda_R/4.1.x
+/usr/bin/time -v Rscript run_SPARKX_mouseHPC_noCovariates.R 2> ../../../outputs/memory/SPARKX_mouseHPC_noCovariates.mem
+
---
+++ analyses/02_run_methods/run_nnSVG/run_nnSVG_mouseHPC_noCovariates.R
@@ -0,0 +1,58 @@
+#######################
+# Script to run method
+# Lukas Weber, Mar 2022
+#######################
+
+# method: nnSVG without covariates
+# dataset: Slide-seqV2 mouse HPC
+
+
+library(SpatialExperiment)
+library(nnSVG)
+library(here)
+
+
+# ---------
+# load data
+# ---------
+
+# load data object with preprocessing from previous script
+
+fn <- here("outputs", "preprocessed", "spe_mouseHPC_preprocessed.rds")
+spe <- readRDS(fn)
+
+dim(spe)
+
+
+# ----------
+# run method
+# ----------
+
+# run method and save results, runtime, peak memory usage
+
+# run nnSVG without covariates
+set.seed(123)
+runtime <- system.time({
+  spe <- nnSVG(
+    spe, 
+    X = NULL, 
+    assay_name = "logcounts", 
+    n_neighbors = 15, 
+    n_threads = 10, 
+    verbose = FALSE
+  )
+})
+
+# store runtime in object
+metadata(spe) <- list(
+  runtime = runtime[["elapsed"]]
+)
+
+
+# -----------
+# save object
+# -----------
+
+file <- here("outputs", "results", "spe_mouseHPC_nnSVG_noCovariates.rds")
+saveRDS(spe, file = file)
+
