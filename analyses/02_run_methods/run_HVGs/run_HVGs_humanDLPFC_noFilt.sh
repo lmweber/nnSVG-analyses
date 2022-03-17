ec103b45cdc748c3897aac29a88096d05b36afc1
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-16 22:33:50 -0400

    remove additional no filtering scenarios

--- analyses/02_run_methods/run_HVGs/run_HVGs_humanDLPFC_noFilt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#$ -cwd
-#$ -l mem_free=10G,h_vmem=12G,h_fsize=100G
-
-module load conda_R/4.1.x
-/usr/bin/time -v Rscript run_HVGs_humanDLPFC_noFilt.R 2> ../../../outputs/memory/HVGs_humanDLPFC_noFilt.mem
-
--- analyses/02_run_methods/run_nnSVG/run_nnSVG_mouseOB_noFilt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#$ -cwd
-#$ -pe local 10
-#$ -l mem_free=3G,h_vmem=4G,h_fsize=100G
-
-module load conda_R/4.1.x
-/usr/bin/time -v Rscript run_nnSVG_mouseOB_noFilt.R 2> ../../../outputs/memory/nnSVG_mouseOB_noFilt.mem
-
