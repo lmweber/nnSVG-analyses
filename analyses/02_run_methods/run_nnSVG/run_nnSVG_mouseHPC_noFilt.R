a6fe8a91855b24c7f37bfe1441d0e78e0ca25821
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-20 01:05:07 -0400

    add no filtering scenario for mouseHPC

---
+++ analyses/02_run_methods/run_nnSVG/run_nnSVG_mouseHPC_noFilt.R
@@ -0,0 +1,64 @@
+#######################
+# Script to run method
+# Lukas Weber, Mar 2022
+#######################
+
+# method: nnSVG with covariates
+# dataset: Slide-seqV2 mouse HPC, without filtering low-expressed genes
+
+
+library(SpatialExperiment)
+library(nnSVG)
+library(here)
+
+
+# ---------
+# load data
+# ---------
+
+# load data object with preprocessing from previous script
+
+fn <- here("outputs", "preprocessed", "spe_mouseHPC_preprocessed_noFilt.rds")
+spe <- readRDS(fn)
+
+dim(spe)
+
+
+# ----------
+# run method
+# ----------
+
+# run method and save results, runtime, peak memory usage
+
+# create model matrix for cell type labels
+X <- model.matrix(~ colData(spe)$celltype)
+dim(X)
+head(X)
+stopifnot(nrow(X) == ncol(spe))
+
+# run nnSVG with covariates
+set.seed(123)
+runtime <- system.time({
+  spe <- nnSVG(
+    spe, 
+    X = X, 
+    assay_name = "logcounts", 
+    n_neighbors = 15, 
+    n_threads = 10, 
+    verbose = FALSE
+  )
+})
+
+# store runtime in object
+metadata(spe) <- list(
+  runtime = runtime[["elapsed"]]
+)
+
+
+# -----------
+# save object
+# -----------
+
+file <- here("outputs", "results", "spe_mouseHPC_nnSVG_noFilt.rds")
+saveRDS(spe, file = file)
+
---
+++ analyses/02_run_methods/run_nnSVG/run_nnSVG_mouseHPC_noFilt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#$ -cwd
+#$ -pe local 10
+#$ -l mem_free=10G,h_vmem=12G,h_fsize=100G
+
+module load conda_R/4.1.x
+/usr/bin/time -v Rscript run_nnSVG_mouseHPC_noFilt.R 2> ../../../outputs/memory/nnSVG_mouseHPC_noFilt.mem
+
