d8399fc99b71a9bcbcdc812733accf7a818e3a3d
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-19 20:32:37 -0400

    scripts to plot top SVGs per method

---
+++ analyses/07_additional_plots/top_SVGs/top_SVGs_humanDLPFC_SPARKX.R
@@ -0,0 +1,91 @@
+############################
+# Additional plots: top SVGs
+# Lukas Weber, Mar 2022
+############################
+
+# data set: human DLPFC
+# filtering: with filtering of low-expressed genes (using nnSVG default filtering)
+
+
+library(SpatialExperiment)
+library(here)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(ggplot2)
+library(ggrepel)
+library(viridis)
+
+
+# directory to save plots
+dir_plots <- here(file.path("plots", "top_SVGs"))
+
+
+# ------------
+# load results
+# ------------
+
+# load as SPE object containing counts
+spe <- readRDS(here("outputs", "results", "spe_humanDLPFC_SPARKX.rds"))
+
+# note filtering
+dim(spe)
+
+
+# -------------------------
+# top SVGs from this method
+# -------------------------
+
+# select top n SVGs
+
+n <- 20
+ix_top <- order(rowData(spe)$rank)[seq_len(n)]
+
+# check gene names
+top_names <- rowData(spe)$gene_name[ix_top]
+top_names
+
+# counts for top n SVGs
+counts_top <- counts(spe)[ix_top, ] %>% as.matrix() %>% t()
+colnames(counts_top) <- top_names
+
+dim(counts_top)
+head(counts_top)
+
+stopifnot(all(rownames(counts_top) == rownames(spatialCoords(spe))))
+
+
+# plot top n SVGs
+
+df <- as.data.frame(cbind(colData(spe), spatialCoords(spe), counts_top)) %>% 
+  select(c("barcode_id", "in_tissue", top_names, 
+           "pxl_col_in_fullres", "pxl_row_in_fullres")) %>% 
+  filter(in_tissue == 1) %>% 
+  pivot_longer(., cols = top_names, 
+               names_to = "gene", values_to = "counts") %>% 
+  mutate(gene = factor(gene, levels = top_names))
+
+max_counts <- max(counts_top)
+max_counts
+
+
+ggplot(df, aes(x = pxl_col_in_fullres, y = pxl_row_in_fullres, color = counts)) + 
+  facet_wrap(~ gene, nrow = 5) + 
+  geom_point(size = 0.05) + 
+  coord_fixed() + 
+  scale_y_reverse() + 
+  scale_color_gradientn(trans = "sqrt", 
+                        colors = c("gray90", mid = "blue", high = "black"), 
+                        breaks = c(0, 150, 195), 
+                        labels = c("0", "", 195)) + 
+  ggtitle("Top SVGs: human DLPFC, SPARK-X") + 
+  theme_bw() + 
+  guides(color = guide_colorbar(ticks = FALSE)) + 
+  theme(panel.grid = element_blank(), 
+        axis.title = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank())
+
+fn <- file.path(dir_plots, "top_SVGs_humanDLPFC_SPARKX")
+ggsave(paste0(fn, ".png"), width = 7, height = 10)
+
