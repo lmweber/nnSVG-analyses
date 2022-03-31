260731a1de83b62e4b55d4396348cd45127e757a
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-31 15:11:53 -0400

    scripts to run Moran's I

---
+++ analyses/02_run_methods/run_MoransI/run_MoransI_humanDLPFC_noFilt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=10G,h_vmem=12G,h_fsize=100G
+
+module load conda_R/4.1.x
+/usr/bin/time -v Rscript run_MoransI_humanDLPFC_noFilt.R 2> ../../../outputs/memory/MoransI_humanDLPFC_noFilt.mem
+
