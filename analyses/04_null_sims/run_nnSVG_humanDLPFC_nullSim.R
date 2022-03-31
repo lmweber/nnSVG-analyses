969d301f8eb91e7f24da81dd422f4bf808191d37
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-31 15:41:13 -0400

    update parameters for null simulations

--- analyses/04_null_sims/run_nnSVG_humanDLPFC_nullSim.R
+++ analyses/04_null_sims/run_nnSVG_humanDLPFC_nullSim.R
@@ -37,7 +37,8 @@ runtime <- system.time({
     spe, 
     X = NULL, 
     assay_name = "logcounts", 
-    n_neighbors = 15, 
+    n_neighbors = 10, 
+    order = "AMMD", 
     n_threads = 10, 
     verbose = FALSE
   )
