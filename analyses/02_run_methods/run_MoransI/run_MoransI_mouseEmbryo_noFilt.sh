df69ba453c75eb9c4e3c1562c761d69b45de91ed
Lukas Weber <lukas.weber.edu@gmail.com>
2022-05-09 16:48:16 -0400

    fix qsub

--- analyses/02_run_methods/run_MoransI/run_MoransI_mouseEmbryo_noFilt.sh
+++ analyses/02_run_methods/run_MoransI/run_MoransI_mouseEmbryo_noFilt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=20G,h_vmem=24G,h_fsize=100G
+#$ -pe local 10
+#$ -l mem_free=10G,h_vmem=12G,h_fsize=100G
 
 module load conda_R/4.1.x
 /usr/bin/time -v Rscript run_MoransI_mouseEmbryo_noFilt.R 2> ../../../outputs/memory/MoransI_mouseEmbryo_noFilt.mem
