081138810ab7062d25eea1f0d52990d6889c640c
Lukas Weber <lukas.weber.edu@gmail.com>
2022-05-12 14:04:42 -0400

    plot cell locations for seqFISH mouse embryo dataset

---
+++ analyses/07_additional_plots/example_genes/plot_cell_locations_mouseEmbryo.R
@@ -0,0 +1,46 @@
+###############################
+# Script to plot cell locations
+# Lukas Weber, May 2022
+###############################
+
+library(SpatialExperiment)
+library(STexampleData)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(here)
+
+
+# directory to save plots
+dir_plots <- here(file.path("plots", "example_genes"))
+
+
+# ------------------
+# Visium human DLPFC
+# ------------------
+
+# Lohoff and Ghazanfar et al. (2021)
+
+spe <- seqFISH_mouseEmbryo()
+
+# cell locations are stored in spatialCoords
+colData(spe)
+head(spatialCoords(spe))
+
+df <- as.data.frame(spatialCoords(spe))
+
+
+# plot cell locations
+ggplot(df, aes(x = x, y = y)) + 
+  geom_point(size = 0.01, color = "gray20") + 
+  coord_fixed() + 
+  ggtitle("Cells: mouse embryo") + 
+  theme_bw() + 
+  theme(panel.grid = element_blank(), 
+        axis.title = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank())
+
+fn <- file.path(dir_plots, "cell_locations_mouseEmbryo")
+ggsave(paste0(fn, ".png"), width = 3, height = 4.25)
+
